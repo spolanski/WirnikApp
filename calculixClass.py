86640b5cf28212bb46a526f404135c449336df03
spolanski <polanski.slawomir@gmail.com>
2016-06-14 00:04:01 +0100

    Nowa aplikacja

--- calculixClass.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun  5 22:18:49 2016
-
-@author: slawek
-"""
-
--- gmsh.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun  5 22:17:12 2016
-
-@author: slawek
-"""
-
